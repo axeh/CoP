b5011560e6e17a7e30d99cc78defb80dac7b901b
Alen Huskic <alen.huskic@outlook.com>
2022-04-22 14:34:01 +0200

    Added new service

---
+++ Code review/CodeReviewDemo/CourseService.cs
@@ -0,0 +1,35 @@
+﻿internal class CourseService
+{
+    private readonly ICourseRepository repository;
+
+    public CourseService(ICourseRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task Add(int id, Student student)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException(null, nameof(id));
+        }
+
+        if (student == null)
+        {
+            throw new ArgumentNullException(nameof(student));
+        }
+
+        var c = repository.Find(id);
+
+        if (c.Students.Any(x => x.Id == student.Id))
+        {
+            throw new ArgumentException("Student already is enrolled for course.");
+        }
+
+        HttpClient httpClient = new HttpClient();
+        var response = await httpClient.GetAsync("/check-student/" + id);
+        response.EnsureSuccessStatusCode();
+
+        c.Students.Add(student);
+    }
+}
